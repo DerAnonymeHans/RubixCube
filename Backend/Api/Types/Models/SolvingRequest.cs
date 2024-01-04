24a1a69e999885a919b18982398e9485088b0e0d
Pascal <pascal.setzer@gmx.de>
2024-01-04 22:32:12 +0100

    Fixed Backend

--- Backend/Api/Types/Models/SolvingRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿using RubixCubeBackend.Types.Enums;
-
-namespace RubixCubeBackend.Types.Models;
-
-internal class SolvingRequest
-{
-    public SolvingAlgorithm Algorithm { get; init; } = SolvingAlgorithm.TwoPhaseSolver;
-}
